443cafc6892fc46f01721488b1801ac7c63c41a7
menna20160441 <mennanosha189@gmail.com>
2020-10-13 03:31:21 +0200

    Relation Creating

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,10 +2,16 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,10 +2,17 @@
 
 namespace App\Model;
 
+use App\Model\Product;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
